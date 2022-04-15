b8517ac7db459b770e4231475d506f44cdc99168
chanyoung <kipa1003@naver.com>
2022-04-15 14:28:00 +0900

    프로젝트 주요 폴더 분류 & 주요 색상 정의

---
+++ resource/colors.ts
@@ -0,0 +1,11 @@
+export const BRAND_COLOR = "#7360BD";
+export const BLACK_100 = "#1F2C37";
+export const BLACK_90 = "#434E58";
+export const BLACK_80 = "#66707A";
+export const BLACK_70 = "#78828A";
+export const BLACK_60 = "#9CA4AB";
+export const BLACK_50 = "#BFC6CC";
+export const BLACK_40 = "#D1D8DD";
+export const BLACK_30 = "#E3E9ED";
+export const BLACK_20 = "#ECF1F6";
+export const BLACK_10 = "#F5F6F7";
