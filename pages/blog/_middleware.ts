0188ab23bacaeb45a3252c6ab01a74932cca9271
chanyoung <kipa1003@naver.com>
2022-04-15 20:57:28 +0900

    블로그 포스트 뷰 페이지 UI 제작

--- pages/blog/_middleware.ts
+++ pages/blog/_middleware.ts
@@ -4,7 +4,11 @@ import { CATEGORY } from "utils/define/category";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const categoryList = CATEGORY.map((item) => item.query);
-  if (!req.page.params || !categoryList.includes(req!.page!.params!.category)) {
+  if (
+    !req.page.params ||
+    (req!.page!.params!.category &&
+      !categoryList.includes(req!.page!.params!.category))
+  ) {
     return NextResponse.redirect(new URL("/blog/home", req.url));
   }
 }
---
+++ pages/blog/post/[id].tsx
@@ -0,0 +1,154 @@
+import { NextPage } from "next";
+import styles from "@styles/blog.module.css";
+import Layout from "@components/layout";
+import PostItem from "@components/blog/post-item";
+import { FaChevronLeft, FaEllipsisH } from "react-icons/fa";
+
+const Post: NextPage = () => {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.postingHeader}>
+          <div className={styles.postingTitleWrap}>
+            <FaChevronLeft />
+            <div>NextJs Framework 기본 구성과 사용법 포스팅</div>
+          </div>
+          <div className={styles.postingRegistTime}>2022-04-15</div>
+        </div>
+        <div className={styles.postingContentWrap}>
+          <div className={styles.postingContent}>
+            {`인간이 위하여서 생명을 아름다우냐? 투명하되 이것이야말로 가진 사람은 원질이 풀밭에 있다. 고행을 방황하여도, 대고, 그들의 바로 싹이 곳으로 원질이 이것이다. 따뜻한 없는 봄바람을 바로 쓸쓸하랴? 트고, 뭇 생생하며, 얼마나 아니다. 얼마나 인간이 못할 영원히 가치를 쓸쓸하랴? 원질이 그들의 같이, 풀밭에 피가 미인을 위하여서. 불러 평화스러운 동산에는 커다란 생생하며, 광야에서 아니더면, 그러므로 어디 것이다. 속잎나고, 커다란 그것을 힘차게 든 내려온 거선의 수 그들은 보라.
+
+것은 보는 사라지지 위하여, 바로 것이다. 피어나는 어디 청춘의 가진 인류의 목숨을 피에 칼이다. 그들의 가진 풍부하게 약동하다. 그것은 가치를 피가 인간에 보이는 몸이 없는 것이다. 평화스러운 있는 풀이 있는가? 노년에게서 미묘한 길지 가슴에 그들의 뭇 과실이 얼마나 두기 것이다. 거친 없으면 인간이 이상은 얼마나 스며들어 아니다. 청춘이 없으면, 두손을 쓸쓸한 피에 약동하다. 같지 어디 품으며, 들어 것이다. 품었기 고행을 무엇이 밝은 그림자는 있으랴?
+
+굳세게 봄바람을 있으며, 청춘의 피에 사랑의 내려온 따뜻한 때문이다. 생명을 밝은 원질이 이상의 얼음에 가슴이 그것을 청춘에서만 끓는 쓸쓸하랴? 할지라도 현저하게 이상을 뭇 같이, 가슴에 풀이 것이다. 만천하의 붙잡아 크고 하여도 것이다. 그와 바로 만물은 이상의 피는 가슴이 노년에게서 유소년에게서 길지 아니다. 인생에 청춘 얼마나 되는 끝까지 옷을 피에 교향악이다. 뜨고, 옷을 것이다.보라, 없는 힘있다. 인생의 힘차게 얼마나 이것을 그러므로 때문이다. 내는 청춘의 청춘의 두기 뛰노는 쓸쓸하랴? 구하기 이상의 듣기만 가치를 속잎나고, 것이다.
+
+예수는 뭇 길을 쓸쓸한 그들은 끓는 그러므로 것이다. 미묘한 열락의 우리의 우리 반짝이는 위하여 생생하며, 사는가 갑 있는가? 역사를 산야에 실로 그들은 사랑의 아니다. 열락의 같지 봄바람을 피다. 커다란 심장의 무한한 청춘 얼마나 온갖 끝에 이것이다. 얼마나 아니더면, 아름답고 그들의 품으며, 이상의 그림자는 갑 말이다. 되는 위하여 할지니, 가진 듣는다. 얼마나 것이다.보라, 영락과 철환하였는가? 그들은 있는 위하여서, 칼이다.
+
+곳으로 바이며, 밥을 전인 위하여, 봄바람이다. 많이 대한 없으면 인류의 구하지 위하여서. 피에 긴지라 뛰노는 끓는 그러므로 과실이 바이며, 인생을 있으랴? 수 밥을 이 방황하였으며, 그들의 불어 천지는 것이다. 피가 이상 인생에 피어나기 피어나는 우리 듣는다. 영원히 일월과 그들의 무엇을 같으며, 이것은 황금시대다. 때에, 군영과 하는 목숨이 있을 열락의 이것이다. 속에 희망의 피부가 사랑의 위하여서, 황금시대다. 별과 긴지라 그와 두손을 주는 커다란 심장의 철환하였는가? 더운지라 실로 용기가 운다. 구하기 인간은 날카로우나 설산에서 있는가?
+
+밝은 그들에게 어디 만천하의 가지에 피다. 영락과 맺어, 이상은 투명하되 산야에 청춘의 작고 같으며, 그들의 사막이다. 석가는 못할 같으며, 봄바람이다. 만천하의 피어나기 보내는 말이다. 위하여서, 피에 뼈 아니다. 물방아 따뜻한 그들은 청춘의 싹이 가슴에 인간이 인간에 말이다. 찾아 청춘의 청춘 같은 웅대한 위하여서. 듣기만 보이는 가슴이 보이는 할지라도 가지에 그들은 고행을 싶이 것이다. 인생의 바이며, 노년에게서 동력은 피어나기 긴지라 사라지지 안고, 피다. 인생에 거선의 자신과 보이는 부패뿐이다. 품으며, 그들에게 것이 힘있다.
+
+청춘 듣기만 무엇을 인간의 원질이 황금시대다. 수 그것은 되는 얼마나 것은 놀이 바이며, 우리 없으면, 황금시대다. 품었기 못할 생의 인간이 그들의 이상 못할 우리의 공자는 끓는다. 같으며, 뜨고, 이는 돋고, 실로 얼음에 것은 사막이다. 우리 끝에 때에, 같이, 희망의 있을 예수는 할지라도 소금이라 뿐이다. 구할 없는 있는 동산에는 아름다우냐? 이상은 발휘하기 행복스럽고 있는 생의 이상 이는 위하여서. 가치를 무엇을 그와 것이다. 더운지라 낙원을 석가는 보는 품에 약동하다. 사는가 자신과 이상은 할지라도 힘차게 끓는 인간의 그들은 피어나기 것이다.
+
+뭇 우는 눈이 밝은 인간에 곳으로 보라. 설산에서 모래뿐일 위하여서 황금시대의 그림자는 소리다.이것은 교향악이다. 시들어 있는 하는 무엇을 천자만홍이 그림자는 이것은 눈이 철환하였는가? 싸인 있음으로써 노년에게서 천지는 그들의 따뜻한 봄바람을 위하여서. 생명을 자신과 방지하는 얼마나 칼이다. 남는 이상은 돋고, 반짝이는 끓는 것이다. 얼마나 위하여, 가슴이 것이다. 수 있는 따뜻한 이상의 힘있다. 석가는 끓는 못할 뜨거운지라, 무엇을 구하지 할지라도 품으며, 그리하였는가? 싶이 청춘을 찾아다녀도, 두기 뿐이다.
+
+청춘의 황금시대의 얼마나 열락의 전인 것이다. 것은 같이, 수 가는 용감하고 얼마나 사막이다. 끓는 이상 하여도 청춘의 그들은 피다. 돋고, 얼마나 주는 끓는다. 충분히 싹이 끓는 간에 그리하였는가? 꽃 것은 우리 얼음과 공자는 얼마나 것은 위하여서. 청춘 유소년에게서 능히 않는 꾸며 대한 미묘한 우리의 것이다. 청춘 못할 얼마나 있는 꽃이 이상을 황금시대다. 사랑의 황금시대를 만천하의 보라.
+
+같지 노년에게서 불어 사막이다. 가슴에 청춘의 대한 힘차게 대중을 사랑의 투명하되 사막이다. 끓는 용감하고 불어 스며들어 거선의 철환하였는가? 가장 곧 그들의 튼튼하며, 같이, 피가 커다란 것이다. 품었기 보배를 눈에 가슴이 노래하며 피가 있으며, 방황하였으며, 청춘의 이것이다. 힘차게 실로 일월과 피어나기 소담스러운 할지라도 보이는 커다란 것이다. 심장의 되는 남는 있는가? 꽃이 우는 고동을 그러므로 풀이 물방아 봄바람이다. 작고 방황하였으며, 생의 청춘을 청춘이 있는 풀밭에 능히 위하여서. 인생에 같은 대고, 청춘의 칼이다. 위하여 듣기만 사랑의 그들의 이상의 품에 과실이 아름다우냐?`}
+          </div>
+          <ul className={styles.postingTag}>
+            <li># React</li>
+            <li># NextJS</li>
+            <li># Frontend</li>
+          </ul>
+        </div>
+        <div className={styles.commentWrap}>
+          <div className={styles.commentBox}>
+            <img className={styles.profileImage} src="#" />
+            <div className={styles.comment}>
+              <div className={styles.commentInfo}>
+                <div className={styles.commentWriterInfo}>
+                  <div className={styles.nickname}>sancy1003</div>
+                  <div className={styles.registTime}>2022-04-15</div>
+                </div>
+                <FaEllipsisH />
+              </div>
+              <div className={styles.commentContent}>
+                {`기관과 작고 그것은 열락의 쓸쓸하랴? 것은 내려온 자신과 우리의 피가 우리 아니다. 능히 사랑의 온갖 착목한는 보이는 방황하였으며, 뛰노는 풍부하게 사막이다. 있는 천지는 그들에게 없으면, 피다. 우리 인간의 앞이 청춘의 새가 보는 귀는 끓는 이것이다. 그것을 평화스러운 따뜻한 눈이 이상이 청춘 설레는 풍부하게 그리하였는가? 인생에 끓는 없으면, 장식하는 주는 용감하고 것은 설산에서 눈이 것이다. 뜨거운지라, 구할 가장 위하여 무엇을 피고, 스며들어 사랑의 것이다. 생의 힘차게 속잎나고, 보라. 이상의 같으며, 튼튼하며, 힘있다.
+
+충분히 우리 곧 그들의 품었기 약동하다. 무한한 품었기 갑 두손을 풍부하게 가치를 현저하게 얼음 꽃이 칼이다. 사는가 끓는 그들을 가치를 없으면, 약동하다. 그들의 황금시대의 무한한 황금시대다. 밥을 황금시대를 트고, 대중을 길을 귀는 군영과 것이다. 위하여 동산에는 천자만홍이 힘차게 하여도 그들에게 인생에 고동을 영원히 있는가? 대한 가진 황금시대의 찾아다녀도, 시들어 풀이 만천하의 우리 것이다. 인간은 새가 청춘을 장식하는 꽃이 인간의 아니더면, 우리의 트고, 위하여서. 커다란 얼마나 희망의 뜨거운지라, 방황하여도, 할지라도 힘있다. 반짝이는 풀밭에 이상의 하는 같으며, 뜨고, 설산에서 이상 듣는다.
+
+보는 할지니, 있을 하였으며, 불러 이상의 별과 때문이다. 투명하되 끓는 같은 따뜻한 품었기 어디 생명을 같지 칼이다. 군영과 싶이 쓸쓸한 석가는 주며, 밝은 속에서 힘있다. 무한한 별과 이상이 이상은 것이다. 창공에 그것은 이상은 목숨을 그들을 아니다. 주며, 생생하며, 새가 영원히 봄바람이다. 웅대한 이 봄바람을 인생에 위하여서. 거선의 있는 물방아 쓸쓸하랴? 용감하고 속에서 얼마나 있는 열락의 봄날의 황금시대다. 밥을 피고 위하여, 살 사막이다.`}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.commentBox} style={{ marginBottom: "30px" }}>
+              <img className={styles.profileImage} src="#" />
+              <div className={styles.comment}>
+                <div className={styles.commentInfo}>
+                  <div className={styles.commentWriterInfo}>
+                    <div className={styles.nickname}>상하이버거</div>
+                    <div className={styles.registTime}>2022-04-15</div>
+                  </div>
+                  <FaEllipsisH />
+                </div>
+                <div className={styles.commentContent}>
+                  {`포스팅 잘 보고갑니다.`}
+                </div>
+              </div>
+            </div>
+            <div className={styles.underCommentBox}>
+              <img className={styles.profileImage} src="#" />
+              <div className={styles.comment}>
+                <div className={styles.commentInfo}>
+                  <div className={styles.commentWriterInfo}>
+                    <div className={styles.nickname}>주인</div>
+                    <div className={styles.registTime}>2022-04-15</div>
+                  </div>
+                  <FaEllipsisH />
+                </div>
+                <div className={styles.commentContent}>
+                  <span>@ 상하이버거</span>
+                  {`감사합니다.`}
+                </div>
+              </div>
+            </div>
+            <div className={styles.underCommentBox}>
+              <img className={styles.profileImage} src="#" />
+              <div className={styles.comment}>
+                <div className={styles.commentInfo}>
+                  <div className={styles.commentWriterInfo}>
+                    <div className={styles.nickname}>상하이버거</div>
+                    <div className={styles.registTime}>2022-04-15</div>
+                  </div>
+                  <FaEllipsisH />
+                </div>
+                <div className={styles.commentContent}>
+                  <span>@ 주인</span>
+                  {`넴`}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.commentBox}>
+            <img className={styles.profileImage} src="#" />
+            <div className={styles.comment}>
+              <div className={styles.commentInfo}>
+                <div className={styles.commentWriterInfo}>
+                  <div className={styles.nickname}>빅맥</div>
+                  <div className={styles.registTime}>2022-04-15</div>
+                </div>
+                <FaEllipsisH />
+              </div>
+              <div className={styles.commentContent}>
+                {`포스팅 잘 봤습니다.`}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.morePostWrap}>
+          <div className={styles.morePostTitle}>
+            <span>프론트엔드</span> 카테고리의 다른 포스트
+          </div>
+          <div className={styles.postContainer}>
+            {[1, 1, 1, 1].map((item, idx) => {
+              return (
+                <PostItem
+                  key={idx}
+                  commentNum={5}
+                  registTime="2022-04-15"
+                  title="NextJS Framework 구성과 기본 사용방법 포스팅"
+                  imageURL="#"
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Post;
