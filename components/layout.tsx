3c750c8e4784ff5f83a9f8c8ee7985ee12773767
chanyoung <kipa1003@naver.com>
2022-04-15 15:20:35 +0900

    기본 레이아웃 추가 & 기본 폰트 적용

---
+++ components/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import styles from "@styles/Layout.module.css";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+export default function Layout({ title, children }: LayoutProps) {
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - chanstory` : `chanstory`}</title>
+      </Head>
+      <div className={styles.header}>
+        <div className={`${styles.logo} BRAND_COLOR`}>Chanstory</div>
+        <div>Login</div>
+      </div>
+      {children}
+    </div>
+  );
+}
