75e2d91c7718c19c0b51d26596d78ea2e1f89afd
chanyoung <kipa1003@naver.com>
2023-02-10 20:43:09 +0900

    패키지 최신화, eslint, prettier 세팅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,40 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class CustomDocument extends Document {
-  render(): JSX.Element {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
     return (
       <Html lang="ko">
         <Head>
@@ -48,5 +81,3 @@ class CustomDocument extends Document {
     );
   }
 }
-
-export default CustomDocument;
