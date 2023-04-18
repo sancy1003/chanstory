54470ec35916279f41901e9afed129b2be72c1c8
chanyoung <kipa1003@naver.com>
2023-04-18 20:59:39 +0900

    fix: 프로덕션 환경 라우팅 테스트

--- pages/blog/search.tsx
+++ pages/blog/search.tsx
@@ -12,37 +12,35 @@ interface Props {
   postResponse: BlogPostListResponse;
 }
 
-const BlogSearch: NextPage<Props> = () =>
-  // { searchText, postResponse }
-  {
-    return (
-      <Layout activeMenu={'BLOG'}>
-        <S.BlogContainer>
-          <S.BlogContentsContainer>
-            <S.BlogSectionHeader>
-              {/* <S.BlogSearchTitle>{searchText}</S.BlogSearchTitle> */}
-              <SearchInput />
-            </S.BlogSectionHeader>
-            <S.BlogSection>
-              {/* <S.PostContainer>
-                {postResponse.posts.map((post) => {
-                  return (
-                    <PostItem
-                      key={post.id}
-                      createdAt={post.createdAt}
-                      title={post.title}
-                      imageURL={post.thumbnailURL}
-                      postId={post.id}
-                    />
-                  );
-                })}
-              </S.PostContainer> */}
-            </S.BlogSection>
-          </S.BlogContentsContainer>
-        </S.BlogContainer>
-      </Layout>
-    );
-  };
+const BlogSearch: NextPage<Props> = ({ searchText, postResponse }) => {
+  return (
+    <Layout activeMenu={'BLOG'}>
+      <S.BlogContainer>
+        <S.BlogContentsContainer>
+          <S.BlogSectionHeader>
+            <S.BlogSearchTitle>{searchText}</S.BlogSearchTitle>
+            <SearchInput />
+          </S.BlogSectionHeader>
+          <S.BlogSection>
+            <S.PostContainer>
+              {postResponse.posts.map((post) => {
+                return (
+                  <PostItem
+                    key={post.id}
+                    createdAt={post.createdAt}
+                    title={post.title}
+                    imageURL={post.thumbnailURL}
+                    postId={post.id}
+                  />
+                );
+              })}
+            </S.PostContainer>
+          </S.BlogSection>
+        </S.BlogContentsContainer>
+      </S.BlogContainer>
+    </Layout>
+  );
+};
 
 type Querys = {
   searchText: string;
@@ -52,46 +50,46 @@ interface Context extends GetServerSidePropsContext {
   query: Querys;
 }
 
-// export async function getServerSideProps({ query }: Context) {
-//   const { searchText } = query;
+export async function getServerSideProps({ query }: Context) {
+  const { searchText } = query;
 
-//   const posts = await client.post.findMany({
-//     where: {
-//       isHide: false,
-//       type: 'POST',
-//       OR: [
-//         {
-//           title: {
-//             contains: searchText,
-//           },
-//         },
-//         {
-//           content: {
-//             contains: searchText,
-//           },
-//         },
-//       ],
-//     },
-//     select: {
-//       id: true,
-//       title: true,
-//       createdAt: true,
-//       thumbnailURL: true,
-//       category: true,
-//     },
-//     orderBy: { createdAt: 'desc' },
-//   });
+  const posts = await client.post.findMany({
+    where: {
+      isHide: false,
+      type: 'POST',
+      OR: [
+        {
+          title: {
+            contains: searchText,
+          },
+        },
+        {
+          content: {
+            contains: searchText,
+          },
+        },
+      ],
+    },
+    select: {
+      id: true,
+      title: true,
+      createdAt: true,
+      thumbnailURL: true,
+      category: true,
+    },
+    orderBy: { createdAt: 'desc' },
+  });
 
-//   const postResponse = {
-//     posts: posts.map((post) => ({
-//       ...post,
-//       createdAt: dateToStringFromServer(post.createdAt),
-//     })),
-//   };
+  const postResponse = {
+    posts: posts.map((post) => ({
+      ...post,
+      createdAt: dateToStringFromServer(post.createdAt),
+    })),
+  };
 
-//   return {
-//     props: { searchText, postResponse },
-//   };
-// }
+  return {
+    props: { searchText, postResponse },
+  };
+}
 
 export default BlogSearch;
--- styles/components/common/layout.style.ts
+++ styles/components/common/layout.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
+  width: 100%;
   min-height: 100vh;
   background-color: #ffffff;
 `;
